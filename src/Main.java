d59ff6435a1645fda7e126ee89f1ca60078fdef1
Ludmila Nistor <ludmila.nistor@nsih.local>
2023-09-28 17:39:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+      long CurrentWorldPopulation =781100808;
+      int CarsProducedThisYear= 5525358;
+      char FirstLetterInAlphabet = 'A';
+     //  Pi = 3.14159;
+
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+          System.out.println(FirstLetterInAlphabet);
+        }
+    }
+
+
